cd0f425a76281bedc065b8c2795794a76aaea111
tmurakami <tmurakami@users.noreply.github.com>
2017-01-20 21:22:10 +0900

    Update Java version to 7

--- classinjector/src/main/java/com/github/tmurakami/classinjector/ClassSources.java
+++ classinjector/src/main/java/com/github/tmurakami/classinjector/ClassSources.java
@@ -7,6 +7,7 @@ import java.util.List;
 /**
  * An object representing a group of {@link ClassSource} objects.
  */
+@SuppressWarnings("WeakerAccess")
 public final class ClassSources implements ClassSource {
 
     private final List<ClassSource> sources;
@@ -20,7 +21,7 @@ public final class ClassSources implements ClassSource {
         if (sources == null) {
             throw new IllegalArgumentException("'sources' is null");
         }
-        List<ClassSource> list = new ArrayList<ClassSource>();
+        List<ClassSource> list = new ArrayList<>();
         for (ClassSource s : sources) {
             if (s == null) {
                 throw new IllegalArgumentException("'sources' contains null");
